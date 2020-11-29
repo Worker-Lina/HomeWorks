2e284de86e1e5457d921d2ef544161715b68ae84
алина человечкова <lina.chelovechkova@mail.ru>
2020-11-29 15:57:09 +0600

    Добавьте файлы проекта.

---
+++ Homework1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alina Chelovechkova!");
+            Console.WriteLine("Введите первое число");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int secondNumber = int.Parse(Console.ReadLine());
+            int summa = firstNumber + secondNumber;
+            Console.WriteLine($" { firstNumber} + { secondNumber} = { summa}");
+        }
+    }
+}
